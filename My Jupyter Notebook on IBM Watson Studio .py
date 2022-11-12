ebab5d40693e56429a28c22637dd7cbf5d9e5689
lguimond <117326505+lguimond@users.noreply.github.com>
2022-11-11 18:05:32 -0700

    Add files via upload

---
+++ My Jupyter Notebook on IBM Watson Studio .py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/Logos/organization_logo/organization_logo.png" width="300" alt="cognitiveclass.ai logo"  />
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Laura Guimond**
+# Mathematics Teacher
+
+# *I am interested in data science because I am looking to switch careers at the end of the school year. I also find learning new skills fun and an engaging way to stimulate my brain.*
+
+# ### The code below will divide 12 by 4
+
+# In[1]:
+
+
+12/4
+
+
+# ![thanksgiving%20photo.jpg](attachment:thanksgiving%20photo.jpg)
+# 
+# 
+# | Name  | Item to Bring for Thanksgiving |
+# | ------| -------------------------------|
+# | Jane  |Turkey                          |
+# | John  | Apple Pie.                     |
+# | Siddhartha | Mac n Cheese              |
+# | Bethany | Drinks                       |
+# | Cole   | Green Bean Casserole          |
+# | ~~Sally~~ | ~~Sweet Potatoe Casserole.~~    |
+# | Camile | Sweet Potatoe Casserole |
+# 
+
+# In[ ]:
+
+
+
+
